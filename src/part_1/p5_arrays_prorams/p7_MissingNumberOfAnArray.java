ddba7ae1745c6a9192cb94758f897ca7ef5295a9
Chethan MS <chethan.pu16@gmail.com>
2025-11-06 00:22:05 +0530

    commit 1

---
+++ src/part_1/p5_arrays_prorams/p7_MissingNumberOfAnArray.java
@@ -0,0 +1,23 @@
+package part_1.p5_arrays_prorams;
+
+import java.net.SocketTimeoutException;
+
+public class p7_MissingNumberOfAnArray {
+
+    public static void main(String[] args) {
+        int number[]={1,2,4,5,6};
+        int totalnumbers=6;
+        int totalsumofnumbers=totalnumbers*(totalnumbers+1)/2;
+        System.out.println(totalsumofnumbers);
+        int sum=0;
+
+        for(int i=0;i<number.length;i++)
+        {
+            sum=number[i]+sum;
+        }
+        System.out.println(sum);
+        System.out.println("The missing number : "+(totalsumofnumbers-sum));
+
+    }
+}
+
