0825ffd5b8985d2b4429d294ba6eae8597655b1e
Chethan MS <chethan.pu16@gmail.com>
2025-12-13 01:09:02 +0530

    dec 13

---
+++ src/important_programs/I4_ReverseTheFirstWordInGivenString.java
@@ -0,0 +1,33 @@
+package important_programs;
+
+public class I4_ReverseTheFirstWordInGivenString
+{
+    public static void main(String[] args) {
+        String input="Hello Chethan Hi";
+        reversefirstword(input);
+    }
+    public static void reversefirstword(String input) {
+        if (input == null || input.isEmpty())
+        {
+            System.out.println(" Can't convert the given string");
+        }
+        else{
+           String []  words = input.split(" ",2);
+            char []charArray =  words[0].toCharArray();
+           int left=0;
+           int right=charArray.length-1;
+           while(left<right)
+           {
+               char temp=charArray[left];
+               charArray[left]=charArray[right];
+               charArray[right]=temp;
+               left++;
+               right--;
+           }
+           String result=new String(charArray);
+            System.out.println("OG : "+ input);
+            System.out.println(result+" "+words[1]);
+        }
+
+    }
+}
---
+++ src/important_programs/I5_CountUpperAndLowerCaseInString.java
@@ -0,0 +1,32 @@
+package important_programs;
+
+public class I5_CountUpperAndLowerCaseInString {
+    public static void main(String[] args) {
+        String input ="ChetHaN";
+        countupperandlower(input);
+    }
+
+    public static void countupperandlower(String input) {
+        if (input == null || input.isEmpty()) {
+            System.out.println(" Can't convert the given string");
+        } else {
+
+
+            char[] charArray = input.toCharArray();
+            int upper = 0;
+            int Lower = 0;
+            for (Character temp : charArray) {
+                if (Character.isUpperCase(temp)) {
+                    upper++;
+                } else if (Character.isLowerCase(temp)) {
+                    Lower++;
+                }
+
+
+            }
+            System.out.println( "No of Upper case :"+upper);
+            System.out.println("No of lower case :"+Lower);
+        }
+    }
+}
+
