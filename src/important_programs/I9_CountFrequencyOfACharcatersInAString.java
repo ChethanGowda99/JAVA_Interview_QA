bd33cdcd5d3a4b6817a11438634b114598ec42f2
Chethan MS <chethan.pu16@gmail.com>
2025-12-17 19:33:45 +0530

    dec 15

---
+++ src/important_programs/I9_CountFrequencyOfACharcatersInAString.java
@@ -0,0 +1,23 @@
+package important_programs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class I9_CountFrequencyOfACharcatersInAString {
+    public static void main(String[] args) {
+        String input = "Hello";
+        HashMap<Character, Integer> result = new HashMap<Character, Integer>();
+        char[] datarray = input.toCharArray();
+        for ( Character c:datarray)
+        {
+            result.put(c,result.getOrDefault(c,0)+1);
+        }
+        for(Map.Entry< Character,Integer> entry :result.entrySet())
+        {
+            System.out.println(entry.getKey()+" "+entry.getValue());
+
+        }
+
+    }
+}
+
