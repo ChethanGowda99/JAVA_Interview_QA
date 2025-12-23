c0dfb823685d2efde677317a69b4fa8307bbfe5e
Chethan MS <chethan.pu16@gmail.com>
2025-12-23 23:53:30 +0530

    dec 23

---
+++ src/important_programs/I12_CalculateSumOfAnIntegersInString.java
@@ -0,0 +1,22 @@
+package important_programs;
+
+public class I12_CalculateSumOfAnIntegersInString {
+    public static void main(String[] args) {
+        String input = "This is 1000 and it should be 20 30 40 50 and 100";
+        String[] words = input.split(" ");
+        int sum = 0;
+        for (String word : words) {
+            try {
+                int value = Integer.parseInt(word);
+                sum = sum + value;
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.println("count");
+            }
+
+        }
+        System.out.println(sum);
+
+    }
+}
